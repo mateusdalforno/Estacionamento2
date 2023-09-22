1bd2206e40685f611b7088835ef63679fac0a7ce
mateusdalforno <mateush.dalforno@gmail.com>
2023-09-22 08:35:07 -0300

    Semana 1 - Teste de conexão

---
+++ Estacionamento2/src/connection/Database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS estacionamento;
+USE estacionamento;
+
+CREATE TABLE vaga (
+    idVaga int NOT NULL AUTO_INCREMENT,
+    numero int NOT NULL,
+    rua varchar(50) NOT NULL,
+    obliqua boolean NOT NULL,
+    PRIMARY KEY (idVaga)
+);
